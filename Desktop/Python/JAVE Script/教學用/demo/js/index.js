8ad5a73b64670717c643169e23e795a5fcb5447f
ryan <a0932952126@gmail.com>
2024-02-14 18:17:28 +0800

    樂透產生器完成

--- Desktop/Python/JAVE Script/教學用/demo/js/index.js
+++ Desktop/Python/JAVE Script/教學用/demo/js/index.js
@@ -1,55 +1,88 @@
-// console.log(getlottoNumber(1, 49, 6, true));
-//函式表達式
-// const getBmi2 = (height, weight, point = 2) => (weight / (height / 100)**2).toFixed(point);
-// const randInt = (start, end) => Math.floor(Math.random() * (end - start + 1)) + start;
-
-// users = [
-//     {
-//         name: 'jerry',
-//         email: 'jerry@gmail.com',
-//         password: '12345678',
-//     },
-//     {
-//         name: 'Mary',
-//         email: 'mary@gmail.com',
-//         password: '123456',
-//     },
-//     {
-//         name: 'kevin',
-//         email: 'kevin@gmail.com',
-//         password: '111111',
-//     },
-// ]
-
-// users.forEach((user) => {
-//     document.write(`${user["name"]}<br>${user["email"]}<br>`)    
-// });
-
-// for (let i = 0; i < users.length; i++){
-//     document.write(`${users[i]["name"]}<br>`);
-//     document.write(`${users[i]["email"]}<br>`);
-//     document.write(`${users[i]["password"]}<br>`);
-// }
-
 const dateEl = document.querySelectorAll(".date");
-const lottoEl = document.querySelector("div.lotto-number ul");
+const lottoEl = document.querySelector("div.lotto-number");
 const startEl = document.querySelector("#start");
 console.log(startEl);
 
 const randInt = (start, end) => Math.floor(Math.random() * (end - start + 1)) + start;
 
-dateEl.forEach(element => element.innerText = "2024/01/19");
-
 startEl.addEventListener("click", () => {
-    lottoEl.innerHTML = " ";
+    // 清空元素內容
+    lottoEl.innerHTML = "";
+    dateEl[1].innerHTML=getTime(false);
+    
+    geneLottoTable();
+    // geneLottoLi();
 
-    for (let i = 0; i < 6; i++){
-        const numbers = getLottoNumber(1, 49, 6, true).join(",");
-        lottoEl.innerHTML +=  `<li>${numbers}</li>`;
+const lottoLi = document.querySelectorAll(".lotto-number ul li");
+for (let i = 0 ; i < lottoLi.length; i++ ){
+    if (i % 2 == 0){
+        lottoLi[i].style.color = "orange";
+        lottoLi[i].style.backgrondColor = "black";
+    }
 }
 });
 
-console.log(getBmi(180, 74, 3));
+function geneLottoTable(){
+    let tempStr="<table><tbody>";
+    //<table> <thead><tr><th></th></tr></thead> <tbody><tr><td></td></tr></tbodt></table>
+    for (let i = 0; i < 6; i++){
+        tempStr+="<tr>";
+        const numbers = getLottoNumber(1, 49, 6, true);
+        for(let j = 0; j < numbers.length; j++){
+            tempStr +=  `<td>${numbers[j]}</td>`;
+        }
+        tempStr += "</tr>"; 
+    }
+
+    tempStr+="</tbody></table>";
+    lottoEl.innerHTML = tempStr;
+}
+
+function geneLottoLi(){
+    for (let i = 0; i < 6; i++){
+        const numbers = getLottoNumber(1, 49, 6, true).join(" ");
+        lottoEl.innerHTML +=  `<li>${numbers}</li>`;
+    }
+}
+
+let falsh = true;
+
+function falshLotto(){
+    falsh = !falsh;
+    const lottoLi = document.querySelectorAll(".lotto-number ul li");
+    for (let i = 0 ; i < lottoLi.length; i++ ){
+        if (i % 2 == 0){
+            lottoLi[i].style.color = "orange";
+            if (falsh){
+                lottoLi[i].style.backgrondColor = "black";
+            }else{
+                lottoLi[i].style.backgrondColor = "white";
+            }
+        }
+    }
+}
+
+function getTime(fullTime = true){
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let date = now.getDate();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = String(now.getSeconds()).padStart(2, "0");
+
+    if(fullTime){
+        return `${year}/${month}/${date} ${hours}:${minutes}:${seconds}`;
+    }
+    return `${year}/${month}/${date}`;
+    }
+
+function showTime(){
+    dateEl[0].innerHTML = getTime();
+    setTimeout(() => {
+        showTime();
+     }, 1000);
+}
 
 function getBmi(height, weight, point = 2) {
     return (weight / (height / 100) ** 2).toFixed(point);
